0eb0956e780877e841877f474b6e236cc4530a29
Fausto Spoto <fausto.spoto@h2tcoin.com>
2019-05-11 16:03:26 +0200

    Clean-up

--- takamaka_tests/src/takamaka/tests/CrowdFundingSimplified.java
+++ takamaka_tests/src/takamaka/tests/CrowdFundingSimplified.java
@@ -3,7 +3,6 @@
  */
 package takamaka.tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
