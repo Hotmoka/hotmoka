d01ca09150bbfa8cc9db22ac3632ab52129f20de
Fausto Spoto <fausto.spoto@univr.it>
2019-06-10 22:12:23 +0200

    Fixed test for lambda expressions

--- takamaka_examples/src/takamaka/tests/lambdas/Lambdas.java
+++ takamaka_examples/src/takamaka/tests/lambdas/Lambdas.java
@@ -3,8 +3,8 @@ package takamaka.tests.lambdas;
 import java.math.BigInteger;
 import java.util.function.Function;
 
-import takamaka.lang.Contract;
 import takamaka.lang.Entry;
+import takamaka.lang.ExternallyOwnedAccount;
 import takamaka.lang.Payable;
 import takamaka.lang.PayableContract;
 import takamaka.util.StorageList;
@@ -15,29 +15,24 @@ import takamaka.util.StorageList;
  * also the bootstrap methods of the lambdas and their bridge needs to
  * be modified. This class tests these situations.
  */
-public class Lambdas extends PayableContract {
+public class Lambdas extends ExternallyOwnedAccount {
 	public final BigInteger MINIMUM_INVESTMENT = BigInteger.valueOf(10_000L);
-	private final StorageList<Lambdas> investors = new StorageList<>();
+	private final StorageList<PayableContract> investors = new StorageList<>();
 
-	public @Payable @Entry(Lambdas.class) Lambdas(BigInteger amount) {}
+	public @Payable @Entry Lambdas(BigInteger amount) {}
 
-	public @Payable @Entry(Lambdas.class) void invest(BigInteger amount) {
+	public @Payable @Entry(PayableContract.class) void invest(BigInteger amount) {
 		Lambdas other = new Lambdas(BigInteger.TEN);
 		BigInteger one = BigInteger.ONE;
-		investors.add((Lambdas) caller());
-		System.out.println("FIRST");
+		investors.add((PayableContract) caller());
 		investors.stream().forEach(investor -> investor.receive(MINIMUM_INVESTMENT));
-		System.out.println("SECOND");
 		investors.stream().forEach(investor -> investor.receive(one));
-		System.out.println("THIRD");
 		investors.stream().forEach(other::entry1);
-		System.out.println("FOUR");
 		investors.stream().forEach(investor -> other.entry2(other::entry3));
-		System.out.println("FIVE");
 		investors.stream().forEach(investor -> other.entry4(Lambdas::new));
 	}
 
-	public @Entry void entry1(Lambdas p) {
+	public @Entry void entry1(PayableContract p) {
 		p.receive(1);
 	}
 
