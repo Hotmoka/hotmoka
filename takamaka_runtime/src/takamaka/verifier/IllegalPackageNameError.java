acf523d9d12f9a5670850607ec03b2e0230c85d0
Fausto Spoto <fausto.spoto@univr.it>
2019-08-01 13:19:42 +0200

    Fixed build script

--- takamaka_runtime/src/takamaka/verifier/IllegalPackageNameError.java
+++ takamaka_runtime/src/takamaka/verifier/IllegalPackageNameError.java
@@ -5,6 +5,6 @@ import org.apache.bcel.generic.ClassGen;
 public class IllegalPackageNameError extends Error {
 
 	public IllegalPackageNameError(ClassGen clazz) {
-		super(clazz, "package name is not allowed for class " + clazz.getClassName());
+		super(clazz, "package name is not allowed");
 	}
 }
