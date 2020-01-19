da7275e70d05666ad1178f6a8853389064e62652
Fausto Spoto <fausto.spoto@takamaka.io>
2020-01-19 12:27:07 +0100

    Used project Hotmoka beans

--- takamaka_blockchain/src/io/hotmoka/beans/requests/InitialTransactionRequest.java
+++
@@ -1,7 +0,0 @@
-package io.hotmoka.beans.requests;
-
-import io.hotmoka.beans.annotations.Immutable;
-
-@Immutable
-public interface InitialTransactionRequest extends TransactionRequest {
-}
--- takamaka_blockchain/src/io/hotmoka/beans/requests/TransactionRequest.java
+++
@@ -1,9 +0,0 @@
-package io.hotmoka.beans.requests;
-
-import java.io.Serializable;
-
-import io.hotmoka.beans.annotations.Immutable;
-
-@Immutable
-public interface TransactionRequest extends Serializable {
-}
