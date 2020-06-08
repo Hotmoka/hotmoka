9085b1513fc0985eab59fd6fc071fc60c27fdda6
Fausto Spoto <fausto.spoto@gmail.com>
2020-06-08 18:49:30 +0200

    Divided javadoc into two

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-JAVA_HOME=/usr/lib/jvm/default-java mvn clean install -DskipTests javadoc:aggregate-jar
+JAVA_HOME=/usr/lib/jvm/default-java mvn clean install -DskipTests javadoc:aggregate-jar -pl \!io-takamaka-code
