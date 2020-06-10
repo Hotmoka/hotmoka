7c81a67e7ddcb332ff09831e032c6f9b7b1d3373
Fausto Spoto <fausto.spoto@gmail.com>
2020-06-10 18:23:11 +0200

    Added version number of pom.xml files. Created project for aggregate generation.

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# This Maven invocation will compile and install all Hotmoka and Takamaka
+# jars in your local Maven repository. Change the value for JAVA_HOME
+# to whatever is correct in your machine. Remove -DskipTests if you
+# what to run the tests as well.
+#
+# Its execution generates the following useful files:
+# hotmoka-and-takamaka-assembly/target/hotmoka-and-takamaka-assembly-1.0.0-jar-with-dependencies.jar
+# target/hotmoka-and-takamaka-assembly-1.0.0-javadoc.jar
+#
+# containing all classes and JavaDocs, respectively, of the Hotmoka and Takamaka projects.
+# Moreover, inside each child project, there will be a "target" directory containing the jar of the
+# classes of that project only.
+
 JAVA_HOME=/usr/lib/jvm/default-java mvn clean install -DskipTests javadoc:aggregate-jar
