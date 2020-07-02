394dc9aa8cedc583f9ce14a2b84fb9b622095572
Dinu Berinde <dinu2193@yahoo.com>
2020-07-02 19:41:46 +0200

    added java 9 module to io-hotmoka-network

---
+++ io-hotmoka-network/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module io.hotmoka.network {
+    requires io.hotmoka.nodes;
+    requires org.slf4j;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.beans;
+    requires spring.web;
+    requires spring.context;
+}
