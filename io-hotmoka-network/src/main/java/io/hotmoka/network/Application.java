70079cd475568c87540d4bd204abd7f60fcb6bf6
Fausto Spoto <fausto.spoto@gmail.com>
2020-07-07 21:52:54 +0200

    Simplifications

--- io-hotmoka-network/src/main/java/io/hotmoka/network/Application.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/Application.java
@@ -1,7 +1,5 @@
 package io.hotmoka.network;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,25 +12,17 @@ import io.hotmoka.nodes.Node;
  */
 @SpringBootApplication
 public class Application {
-    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
-    private ConfigurableApplicationContext configurableApplicationContext;
 
-
-    public static void main(String[] args) {
-        // needed by spring
-        throw new IllegalStateException("Cannot run NodeService application as a standalone jar");
-    }
-
-    /**
+	/**
      * Start the Spring boot application and register the node {@link io.hotmoka.nodes.Node} to the beans map of the Spring framework in order
      * to be accessible by the application
      * @param config the configuration of the application
      * @param node the node
      */
-    public void start(Config config, Node node) {
-        LOGGER.info("Starting NodeService application");
-        this.configurableApplicationContext = SpringApplication.run(Application.class, springArgumentsFor(config));
-        this.configurableApplicationContext.getBeanFactory().registerSingleton("node", node);
+    public ConfigurableApplicationContext start(Config config, Node node) {
+    	ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, springArgumentsFor(config));
+    	configurableApplicationContext.getBeanFactory().registerSingleton("node", node);
+    	return configurableApplicationContext;
     }
 
     /**
@@ -46,34 +36,5 @@ public class Application {
    			"--server.port=" + config.port,
    			"--spring.main.banner-mode=" + (config.showSpringBanner ? Banner.Mode.CONSOLE : Banner.Mode.OFF)
     	};
-    }
-
-    /**
-     * Shutdown the Spring boot application
-     */
-    public void stop() {
-        int times = 3;
-        int counter = 0;
-        int halfMinute = 30000;
-
-        while (counter++ <= times) {
-
-            if (this.configurableApplicationContext != null) {
-                LOGGER.info("Stopping NodeService application");
-                SpringApplication.exit(configurableApplicationContext);
-                break;
-            } else {
-
-                try {
-                    Thread.sleep(halfMinute);
-                } catch (InterruptedException e) {
-                    LOGGER.error("Error stopping NodeService application", e);
-                }
-            }
-        }
-    }
-
-    public ConfigurableApplicationContext getConfigurableApplicationContext() {
-        return this.configurableApplicationContext;
     }
 }
--- io-hotmoka-network/src/main/java/io/hotmoka/network/internal/NodeServiceImpl.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/internal/NodeServiceImpl.java
@@ -1,5 +1,10 @@
 package io.hotmoka.network.internal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 import io.hotmoka.network.Application;
 import io.hotmoka.network.Config;
 import io.hotmoka.network.NodeService;
@@ -9,7 +14,8 @@ import io.hotmoka.nodes.Node;
  * A simple web service that exposes some REST APIs to access an instance of a {@link io.hotmoka.nodes.Node}.
  */
 public class NodeServiceImpl implements NodeService {
-    private final Application application;
+	private final ConfigurableApplicationContext context;
+    private final static Logger LOGGER = LoggerFactory.getLogger(NodeServiceImpl.class);
 
 	/**
 	 * Yields an implementation of a network service that exposes a REST API to a given Hotmoka node.
@@ -19,12 +25,14 @@ public class NodeServiceImpl implements NodeService {
 	 * @return the network service implementation
 	 */
     public NodeServiceImpl(Config config, Node node) {
-        this.application = new Application();
-        this.application.start(config, node);
+    	Application application = new Application();
+        context = application.start(config, node);
+        LOGGER.info("Network server for Hotmoka node started");
     }
 
     @Override
     public void close() {
-    	application.stop();
+    	SpringApplication.exit(context);
+    	LOGGER.info("Network server for Hotmoka node closed");
     }
 }
