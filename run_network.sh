3d2439e2a39dbd2a520150ab0b07c015de4e2e31
Dinu Berinde <dinu2193@yahoo.com>
2020-07-28 00:50:42 +0200

    run network shell script compatible with windows

--- run_network.sh
+++ run_network.sh
@@ -1,1 +1,5 @@
-java --module-path modules/explicit:modules/automatic --class-path "modules/unnamed/*" --module io.hotmoka.network/io.hotmoka.network.runs.Main
+cwd=$(pwd)
+explicitModules=$cwd"/modules/explicit"
+automaticModules=$cwd"/modules/automatic"
+
+java --module-path $explicitModules:$automaticModules --class-path "modules/unnamed/*" --module io.hotmoka.network/io.hotmoka.network.runs.Main
