32731b777b4f8fc2249fa22250fbc6f868370e33
Dinu Berinde <dinu2193@yahoo.com>
2020-07-29 22:27:32 +0200

    transaction response model implementation

--- io-hotmoka-network/src/main/java/io/hotmoka/network/models/responses/JarStoreInitialTransactionResponseModel.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/models/responses/JarStoreInitialTransactionResponseModel.java
@@ -1,10 +1,33 @@
 package io.hotmoka.network.models.responses;
 
+import io.hotmoka.beans.annotations.Immutable;
 import io.hotmoka.beans.responses.JarStoreInitialTransactionResponse;
+import io.hotmoka.network.models.values.TransactionReferenceModel;
 
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Immutable
 public class JarStoreInitialTransactionResponseModel extends InitialTransactionResponseModel {
 
-    public JarStoreInitialTransactionResponseModel(JarStoreInitialTransactionResponse responseAt) {
-        super();
+    /**
+     * The jar to install, instrumented.
+     */
+    public final String instrumentedJar;
+
+    /**
+     * The dependencies of the jar, previously installed in blockchain.
+     * This is a copy of the same information contained in the request.
+     */
+    public final List<TransactionReferenceModel> dependencies;
+
+    public JarStoreInitialTransactionResponseModel(JarStoreInitialTransactionResponse response) {
+        this.instrumentedJar = Base64.getEncoder().encodeToString(response.getInstrumentedJar());
+        this.dependencies = response.getDependencies().map(TransactionReferenceModel::new).collect(Collectors.toList());
+    }
+
+    public JarStoreInitialTransactionResponse toBean() {
+        return new JarStoreInitialTransactionResponse(Base64.getDecoder().decode(this.instrumentedJar), dependencies.stream().map(TransactionReferenceModel::toBean));
     }
 }
