6b6a9c5f37e3e2514bbe02e3819d1b6eb2d1916a
Dinu Berinde <dinu2193@yahoo.com>
2020-07-31 23:35:58 +0200

    added the remote node test

---
+++ io-takamaka-code-tests/src/test/java/io/takamaka/code/tests/RemoteNode.java
@@ -0,0 +1,65 @@
+package io.takamaka.code.tests;
+
+import io.hotmoka.beans.references.TransactionReference;
+import io.hotmoka.beans.signatures.ConstructorSignature;
+import io.hotmoka.beans.values.StorageReference;
+import io.hotmoka.network.NodeService;
+import io.hotmoka.network.NodeServiceConfig;
+import io.hotmoka.network.RemoteNodeConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.security.PrivateKey;
+
+import static io.hotmoka.beans.types.BasicTypes.INT;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RemoteNode extends TakamakaTest {
+    private static final BigInteger ALL_FUNDS = BigInteger.valueOf(1_000_000_000);
+    private static final BigInteger _20_000 = BigInteger.valueOf(20_000);
+    private static final ConstructorSignature CONSTRUCTOR_INTERNATIONAL_TIME = new ConstructorSignature("io.takamaka.tests.basicdependency.InternationalTime", INT, INT, INT);
+
+    private final NodeServiceConfig configNoBanner = new NodeServiceConfig.Builder().setPort(8080).setSpringBannerModeOn(false).build();
+
+    /**
+     * The account that holds all funds.
+     */
+    private StorageReference master;
+
+    /**
+     * The classpath of the classes being tested.
+     */
+    private TransactionReference classpath;
+
+    /**
+     * The private key of {@linkplain #master}.
+     */
+    private PrivateKey key;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        setNode("basicdependency.jar", ALL_FUNDS, BigInteger.ZERO);
+        master = account(0);
+        key = privateKey(0);
+        classpath = addJarStoreTransaction(key, master, BigInteger.valueOf(10000), BigInteger.ONE, takamakaCode(), bytesOf("basic.jar"), jar());
+    }
+
+    @Test
+    @DisplayName("starts a network server from a Hotmoka node and makes a remote call to getTakamakaCode")
+    void testRemoteTakamakaCode() {
+        NodeServiceConfig config = new NodeServiceConfig.Builder().setPort(8080).setSpringBannerModeOn(true).build();
+        RemoteNodeConfig remoteNodeconfig = new RemoteNodeConfig.Builder().setURL("http://localhost:8080").build();
+
+        try (NodeService nodeRestService = NodeService.of(config, nodeWithJarsView)) {
+            try (io.hotmoka.network.RemoteNode remoteNode = io.hotmoka.network.RemoteNode.of(remoteNodeconfig)) {
+                assertNotNull(remoteNode.getTakamakaCode().getHash());
+            } catch (Exception e) {
+                fail(e.getMessage());
+            }
+        }
+    }
+
+}
