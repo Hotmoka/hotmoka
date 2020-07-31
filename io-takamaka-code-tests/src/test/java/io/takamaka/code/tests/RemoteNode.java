d4f9039eb2c91d849f522d8a818a0a632d88551a
Dinu Berinde <dinu2193@yahoo.com>
2020-08-01 01:32:51 +0200

    added a remote node test to cover a NoSuchElementException

--- io-takamaka-code-tests/src/test/java/io/takamaka/code/tests/RemoteNode.java
+++ io-takamaka-code-tests/src/test/java/io/takamaka/code/tests/RemoteNode.java
@@ -1,5 +1,7 @@
 package io.takamaka.code.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.hotmoka.beans.references.TransactionReference;
 import io.hotmoka.beans.signatures.ConstructorSignature;
 import io.hotmoka.beans.updates.ClassTag;
@@ -7,12 +9,14 @@ import io.hotmoka.beans.values.StorageReference;
 import io.hotmoka.network.NodeService;
 import io.hotmoka.network.NodeServiceConfig;
 import io.hotmoka.network.RemoteNodeConfig;
+import io.hotmoka.network.models.values.StorageReferenceModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.security.PrivateKey;
+import java.util.NoSuchElementException;
 
 import static io.hotmoka.beans.types.BasicTypes.INT;
 import static org.junit.jupiter.api.Assertions.*;
@@ -79,6 +83,37 @@ public class RemoteNode extends TakamakaTest {
 
         assertNotNull(classTag);
         assertEquals(classTag.className, "io.takamaka.code.lang.TestExternallyOwnedAccount");
+    }
+
+    @Test
+    @DisplayName("starts a network server from a Hotmoka node and makes a remote call to getClassTag ending with a NoSuchElementException for a non existing reference")
+    void testRemoteClassTagNoSuchElement() throws Exception {
+        String exceptionType = null;
+
+        try (NodeService nodeRestService = NodeService.of(configNoBanner, nodeWithJarsView)) {
+
+            try (io.hotmoka.network.RemoteNode remoteNode = io.hotmoka.network.RemoteNode.of(remoteNodeconfig)) {
+                JsonObject nonExistentTransactionReferenceJson = new JsonObject();
+                nonExistentTransactionReferenceJson.addProperty("hash", "qwert"); // non existent hash
+                nonExistentTransactionReferenceJson.addProperty("type", "local");
+
+                JsonObject nonExistentStorageReferenceJson = new JsonObject();
+                nonExistentStorageReferenceJson.addProperty("progressive", 0);
+                nonExistentStorageReferenceJson.add("transaction", nonExistentTransactionReferenceJson);
+
+                Gson gson = new Gson();
+                StorageReferenceModel inexistentStorageReferenceModel = gson.fromJson(nonExistentStorageReferenceJson.toString(), StorageReferenceModel.class);
+
+                try {
+                    remoteNode.getClassTag(inexistentStorageReferenceModel.toBean());
+                } catch (NoSuchElementException e) {
+                    exceptionType = e.getClass().getName();
+                }
+            }
+        }
+
+        assertNotNull(exceptionType);
+        assertEquals(exceptionType, NoSuchElementException.class.getName());
     }
 
 }
