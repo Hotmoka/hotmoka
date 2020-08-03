baccc25ea48bfd8ed569e59ab915819d3ff6d2bd
Dinu Berinde <dinu2193@yahoo.com>
2020-08-03 21:25:10 +0200

    added a method to build the corresponding transaction response

--- io-hotmoka-network/src/main/java/io/hotmoka/network/models/responses/TransactionResponseModel.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/models/responses/TransactionResponseModel.java
@@ -31,7 +31,7 @@ public abstract class TransactionResponseModel {
         else if (response instanceof ConstructorCallTransactionSuccessfulResponse)
             return new ConstructorCallTransactionSuccessfulResponseModel((ConstructorCallTransactionSuccessfulResponse) response);
         else if (response instanceof ConstructorCallTransactionExceptionResponse)
-            return new ConstuctorCallTransactionExceptionResponseModel((ConstructorCallTransactionExceptionResponse) response);
+            return new ConstructorCallTransactionExceptionResponseModel((ConstructorCallTransactionExceptionResponse) response);
         else if (response instanceof MethodCallTransactionFailedResponse)
             return new MethodCallTransactionFailedResponseModel((MethodCallTransactionFailedResponse) response);
         else if (response instanceof MethodCallTransactionSuccessfulResponse)
@@ -42,5 +42,42 @@ public abstract class TransactionResponseModel {
             return new MethodCallTransactionExceptionResponseModel((MethodCallTransactionExceptionResponse) response);
         else
             throw new InternalFailureException("unexpected transaction response of class " + response.getClass().getName());
+    }
+
+    /**
+     * Builds the transaction response of the given response model.
+     *
+     * @param responseModel the response model
+     * @return the corresponding transaction response
+     */
+    public static TransactionResponse toBeanFrom(TransactionResponseModel responseModel) {
+        if (responseModel == null)
+            throw new InternalFailureException("unexpected null response model");
+        else if (responseModel instanceof GameteCreationTransactionResponseModel)
+            return ((GameteCreationTransactionResponseModel) responseModel).toBean();
+        else if (responseModel instanceof InitializationTransactionResponseModel)
+            return ((InitializationTransactionResponseModel) responseModel).toBean();
+        else if (responseModel instanceof JarStoreInitialTransactionResponseModel)
+            return ((JarStoreInitialTransactionResponseModel) responseModel).toBean();
+        else if (responseModel instanceof JarStoreTransactionFailedResponseModel)
+            return ((JarStoreTransactionFailedResponseModel) responseModel).toBean();
+        else if (responseModel instanceof JarStoreTransactionSuccessfulResponseModel)
+            return ((JarStoreTransactionSuccessfulResponseModel) responseModel).toBean();
+        else if (responseModel instanceof ConstructorCallTransactionFailedResponseModel)
+            return ((ConstructorCallTransactionFailedResponseModel) responseModel).toBean();
+        else if (responseModel instanceof ConstructorCallTransactionSuccessfulResponseModel)
+            return ((ConstructorCallTransactionSuccessfulResponseModel) responseModel).toBean();
+        else if (responseModel instanceof ConstructorCallTransactionExceptionResponseModel)
+            return ((ConstructorCallTransactionExceptionResponseModel) responseModel).toBean();
+        else if (responseModel instanceof MethodCallTransactionFailedResponseModel)
+            return ((MethodCallTransactionFailedResponseModel) responseModel).toBean();
+        else if (responseModel instanceof MethodCallTransactionSuccessfulResponseModel)
+            return ((MethodCallTransactionSuccessfulResponseModel) responseModel).toBean();
+        else if (responseModel instanceof VoidMethodCallTransactionSuccessfulResponseModel)
+            return ((VoidMethodCallTransactionSuccessfulResponseModel) responseModel).toBean();
+        else if (responseModel instanceof MethodCallTransactionExceptionResponseModel)
+            return ((MethodCallTransactionExceptionResponseModel) responseModel).toBean();
+        else
+            throw new InternalFailureException("unexpected transaction response model of class " + responseModel.getClass().getName());
     }
 }
