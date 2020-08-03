7807fe61a7504897a7ace30405400b3f03490761
Dinu Berinde <dinu2193@yahoo.com>
2020-08-03 23:41:32 +0200

    implemented the remaining remote node methods

--- io-hotmoka-network/src/main/java/io/hotmoka/network/internal/RemoteNodeImpl.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/internal/RemoteNodeImpl.java
@@ -16,13 +16,12 @@ import io.hotmoka.network.RemoteNode;
 import io.hotmoka.network.RemoteNodeConfig;
 import io.hotmoka.network.internal.services.NetworkExceptionResponse;
 import io.hotmoka.network.internal.services.RestClientService;
-import io.hotmoka.network.models.requests.JarStoreInitialTransactionRequestModel;
-import io.hotmoka.network.models.requests.JarStoreTransactionRequestModel;
-import io.hotmoka.network.models.requests.TransactionRequestModel;
+import io.hotmoka.network.models.requests.*;
 import io.hotmoka.network.models.responses.TransactionResponseModel;
 import io.hotmoka.network.models.updates.ClassTagModel;
 import io.hotmoka.network.models.updates.StateModel;
 import io.hotmoka.network.models.values.StorageReferenceModel;
+import io.hotmoka.network.models.values.StorageValueModel;
 import io.hotmoka.network.models.values.TransactionReferenceModel;
 
 import java.security.NoSuchAlgorithmException;
@@ -53,6 +52,7 @@ public class RemoteNodeImpl implements RemoteNode {
 			return RestClientService.get(config.url + "/get/takamakaCode", TransactionReferenceModel.class).toBean();
 		}
 		catch (NetworkExceptionResponse exceptionResponse) {
+
 			if (exceptionResponse.getExceptionType().equals(NoSuchElementException.class.getName()))
 				throw new NoSuchElementException(exceptionResponse.getMessage());
 
@@ -66,6 +66,7 @@ public class RemoteNodeImpl implements RemoteNode {
 			return RestClientService.get(config.url + "/get/manifest", StorageReferenceModel.class).toBean();
 		}
 		catch (NetworkExceptionResponse exceptionResponse) {
+
 			if (exceptionResponse.getExceptionType().equals(NoSuchElementException.class.getName()))
 				throw new NoSuchElementException(exceptionResponse.getMessage());
 
@@ -79,6 +80,7 @@ public class RemoteNodeImpl implements RemoteNode {
 			return RestClientService.post(config.url + "/get/classTag", new StorageReferenceModel(reference), ClassTagModel.class).toBean(reference);
 		}
 		catch (NetworkExceptionResponse exceptionResponse) {
+
 			if (exceptionResponse.getExceptionType().equals(NoSuchElementException.class.getName()))
 				throw new NoSuchElementException(exceptionResponse.getMessage());
 
@@ -92,6 +94,7 @@ public class RemoteNodeImpl implements RemoteNode {
 			return RestClientService.post(config.url + "/get/state", new StorageReferenceModel(reference), StateModel.class).toBean(reference);
 		}
 		catch (NetworkExceptionResponse exceptionResponse) {
+
 			if (exceptionResponse.getExceptionType().equals(NoSuchElementException.class.getName()))
 				throw new NoSuchElementException(exceptionResponse.getMessage());
 
@@ -111,6 +114,7 @@ public class RemoteNodeImpl implements RemoteNode {
 			return TransactionRequestModel.toBeanFrom(RestClientService.post(config.url + "/get/requestAt", new TransactionReferenceModel(reference), TransactionRequestModel.class));
 		}
 		catch (NetworkExceptionResponse exceptionResponse) {
+
 			if (exceptionResponse.getExceptionType().equals(NoSuchElementException.class.getName()))
 				throw new NoSuchElementException(exceptionResponse.getMessage());
 
@@ -171,20 +175,44 @@ public class RemoteNodeImpl implements RemoteNode {
 
 	@Override
 	public StorageReference addGameteCreationTransaction(GameteCreationTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/add/gameteCreationTransaction", new GameteCreationTransactionRequestModel(request), StorageReferenceModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public StorageReference addRedGreenGameteCreationTransaction(RedGreenGameteCreationTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/add/redGreenGameteCreationTransaction", new RedGreenGameteCreationTransactionRequestModel(request), StorageReferenceModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public void addInitializationTransaction(InitializationTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
+		try {
+			RestClientService.post(config.url + "/add/initializationTransaction", new InitializationTransactionRequestModel(request), Void.class);
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
 
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
@@ -198,7 +226,7 @@ public class RemoteNodeImpl implements RemoteNode {
 				throw new TransactionRejectedException(exceptionResponse.getMessage());
 
 			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
-				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: refactor the ErrorModel to cover this type of exception
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
 
 			throw new InternalFailureException(exceptionResponse.getMessage());
 		}
@@ -206,55 +234,195 @@ public class RemoteNodeImpl implements RemoteNode {
 
 	@Override
 	public StorageReference addConstructorCallTransaction(ConstructorCallTransactionRequest request) throws TransactionRejectedException, TransactionException, CodeExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/add/constructorCallTransaction", new ConstructorCallTransactionRequestModel(request), StorageReferenceModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			if (exceptionResponse.getExceptionType().equals(CodeExecutionException.class.getName()))
+				throw new CodeExecutionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public StorageValue addInstanceMethodCallTransaction(InstanceMethodCallTransactionRequest request) throws TransactionRejectedException, TransactionException, CodeExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/add/instanceMethodCallTransaction", new InstanceMethodCallTransactionRequestModel(request), StorageReferenceModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			if (exceptionResponse.getExceptionType().equals(CodeExecutionException.class.getName()))
+				throw new CodeExecutionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public StorageValue addStaticMethodCallTransaction(StaticMethodCallTransactionRequest request) throws TransactionRejectedException, TransactionException, CodeExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/add/staticMethodCallTransaction", new StaticMethodCallTransactionRequestModel(request), StorageReferenceModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			if (exceptionResponse.getExceptionType().equals(CodeExecutionException.class.getName()))
+				throw new CodeExecutionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public StorageValue runInstanceMethodCallTransaction(InstanceMethodCallTransactionRequest request) throws TransactionRejectedException, TransactionException, CodeExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/run/instanceMethodCallTransaction", new InstanceMethodCallTransactionRequestModel(request), StorageValueModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			if (exceptionResponse.getExceptionType().equals(CodeExecutionException.class.getName()))
+				throw new CodeExecutionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public StorageValue runStaticMethodCallTransaction(StaticMethodCallTransactionRequest request) throws TransactionRejectedException, TransactionException, CodeExecutionException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return RestClientService.post(config.url + "/run/staticMethodCallTransaction", new StaticMethodCallTransactionRequestModel(request), StorageValueModel.class).toBean();
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			if (exceptionResponse.getExceptionType().equals(TransactionException.class.getName()))
+				throw new TransactionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			if (exceptionResponse.getExceptionType().equals(CodeExecutionException.class.getName()))
+				throw new CodeExecutionException("", exceptionResponse.getMessage(), ""); // TODO: add message constructor
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public JarSupplier postJarStoreTransaction(JarStoreTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return jarSupplierOf(RestClientService.post(config.url + "/post/jarStoreTransaction", new JarStoreTransactionRequestModel(request), TransactionReferenceModel.class).toBean());
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public CodeSupplier<StorageReference> postConstructorCallTransaction(ConstructorCallTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return codeSupplierOf(RestClientService.post(config.url + "/post/constructorCallTransaction", new ConstructorCallTransactionRequestModel(request), TransactionReferenceModel.class).toBean());
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public CodeSupplier<StorageValue> postInstanceMethodCallTransaction(InstanceMethodCallTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return codeSupplierOf(RestClientService.post(config.url + "/post/instanceMethodCallTransaction", new InstanceMethodCallTransactionRequestModel(request), TransactionReferenceModel.class).toBean());
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
 	}
 
 	@Override
 	public CodeSupplier<StorageValue> postStaticMethodCallTransaction(StaticMethodCallTransactionRequest request) throws TransactionRejectedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return codeSupplierOf(RestClientService.post(config.url + "/post/staticMethodCallTransaction", new StaticMethodCallTransactionRequestModel(request), TransactionReferenceModel.class).toBean());
+		}
+		catch (NetworkExceptionResponse exceptionResponse) {
+
+			if (exceptionResponse.getExceptionType().equals(TransactionRejectedException.class.getName()))
+				throw new TransactionRejectedException(exceptionResponse.getMessage());
+
+			throw new InternalFailureException(exceptionResponse.getMessage());
+		}
+	}
+
+	/**
+	 * Yields a {@link io.hotmoka.nodes.Node.CodeSupplier} for the give transaction request reference.
+	 * @param transactionReference the transaction reference
+	 * @return the code supplier
+	 */
+	private static <T extends StorageValue> CodeSupplier<T> codeSupplierOf(TransactionReference transactionReference) {
+		return new CodeSupplier<>() {
+			@Override
+			public TransactionReference getReferenceOfRequest() {
+				return transactionReference;
+			}
+
+			@Override
+			public T get() throws TransactionRejectedException, TransactionException, CodeExecutionException {
+				return null; // TODO: not sure of this
+			}
+		};
+	}
+
+	/**
+	 * Yields a {@link io.hotmoka.nodes.Node.JarSupplier} for the give transaction request reference.
+	 * @param transactionReference the transaction reference
+	 * @return the jar supplier
+	 */
+	private static JarSupplier jarSupplierOf(TransactionReference transactionReference) {
+		return new JarSupplier() {
+			@Override
+			public TransactionReference getReferenceOfRequest() {
+				return transactionReference;
+			}
+
+			@Override
+			public TransactionReference get() throws TransactionRejectedException, TransactionException {
+				return null; // TODO: not sure of this
+			}
+		};
 	}
 }
