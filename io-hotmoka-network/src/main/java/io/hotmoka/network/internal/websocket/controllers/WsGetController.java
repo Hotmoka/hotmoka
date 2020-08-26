56e322eef0d6fc37c9e6f52e13931ad5fa84c805
Dinu Berinde <dinu2193@yahoo.com>
2020-08-26 21:13:15 +0200

    websocket get controller refactor

--- io-hotmoka-network/src/main/java/io/hotmoka/network/internal/websocket/controllers/WsGetController.java
+++ io-hotmoka-network/src/main/java/io/hotmoka/network/internal/websocket/controllers/WsGetController.java
@@ -4,17 +4,11 @@ package io.hotmoka.network.internal.websocket.controllers;
 import io.hotmoka.network.internal.services.GetService;
 import io.hotmoka.network.internal.services.NetworkExceptionResponse;
 import io.hotmoka.network.models.errors.ErrorModel;
-import io.hotmoka.network.models.requests.TransactionRestRequestModel;
-import io.hotmoka.network.models.responses.SignatureAlgorithmResponseModel;
-import io.hotmoka.network.models.responses.TransactionRestResponseModel;
-import io.hotmoka.network.models.updates.ClassTagModel;
-import io.hotmoka.network.models.updates.StateModel;
 import io.hotmoka.network.models.values.StorageReferenceModel;
 import io.hotmoka.network.models.values.TransactionReferenceModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -24,59 +18,53 @@ import java.security.Principal;
 @Controller
 @MessageMapping("/get")
 public class WsGetController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final GetService nodeGetService;
 
     @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-
-    @Autowired
-    private GetService nodeGetService;
+    public WsGetController(SimpMessagingTemplate simpMessagingTemplate, GetService nodeGetService) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.nodeGetService = nodeGetService;
+    }
 
     @MessageMapping("/takamakaCode")
-    @SendTo("/topic/get/takamakaCode")
-    public TransactionReferenceModel getTakamakaCode() {
-        return nodeGetService.getTakamakaCode();
+    public void getTakamakaCode(Principal principal) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/takamakaCode", nodeGetService.getTakamakaCode());
     }
 
     @MessageMapping("/manifest")
-    @SendTo("/topic/get/manifest")
-    public StorageReferenceModel getManifest() {
-        return nodeGetService.getManifest();
+    public void getManifest(Principal principal) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/manifest", nodeGetService.getManifest());
     }
 
     @MessageMapping("/state")
-    @SendTo("/topic/get/state")
-    public StateModel getState(StorageReferenceModel request) {
-        return nodeGetService.getState(request);
+    public void getState(Principal principal, StorageReferenceModel request) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/state", nodeGetService.getState(request));
     }
 
     @MessageMapping("/classTag")
-    @SendTo("/topic/get/classTag")
-    public ClassTagModel getClassTag(StorageReferenceModel request) {
-        return nodeGetService.getClassTag(request);
+    public void getClassTag(Principal principal, StorageReferenceModel request) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/classTag", nodeGetService.getClassTag(request));
     }
 
     @MessageMapping("/request")
-    @SendTo("/topic/get/request")
-    public TransactionRestRequestModel<?> getRequestAt(TransactionReferenceModel reference) {
-        return nodeGetService.getRequest(reference);
+    public void getRequestAt(Principal principal, TransactionReferenceModel reference) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/request", nodeGetService.getRequest(reference));
     }
 
     @MessageMapping("/response")
-    @SendTo("/topic/get/response")
-    public TransactionRestResponseModel<?> getResponseAt(TransactionReferenceModel reference) {
-        return nodeGetService.getResponse(reference);
+    public void getResponseAt(Principal principal, TransactionReferenceModel reference) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/response", nodeGetService.getResponse(reference));
     }
 
     @MessageMapping("/polledResponse")
-    @SendTo("/topic/get/polledResponse")
-    public TransactionRestResponseModel<?> getPolledResponseAt(TransactionReferenceModel reference) {
-        return nodeGetService.getPolledResponse(reference);
+    public void getPolledResponseAt(Principal principal, TransactionReferenceModel reference) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/polledResponse", nodeGetService.getPolledResponse(reference));
     }
 
     @MessageMapping("/signatureAlgorithmForRequests")
-    @SendTo("/topic/get/signatureAlgorithmForRequests")
-    public SignatureAlgorithmResponseModel getSignatureAlgorithmForRequests() {
-        return nodeGetService.getSignatureAlgorithmForRequests();
+    public void getSignatureAlgorithmForRequests(Principal principal) {
+        simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/get/signatureAlgorithmForRequests", nodeGetService.getSignatureAlgorithmForRequests());
     }
 
     @MessageExceptionHandler
