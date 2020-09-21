c32c7eab9aaaeebf5d8e7301ee032b4e0ddb58e5
Fausto Spoto <fausto.spoto@gmail.com>
2020-09-21 18:27:43 +0200

    Fixed module descriptor

--- io-hotmoka-network/src/main/java/module-info.java
+++ io-hotmoka-network/src/main/java/module-info.java
@@ -15,7 +15,7 @@ module io.hotmoka.network {
 	exports io.hotmoka.network.internal.websockets.controllers to spring.beans;
 	opens io.hotmoka.network.internal to spring.core;
     opens io.hotmoka.network.internal.services to spring.core; //, com.google.gson;
-    opens io.hotmoka.network.internal.http.controllers to spring.core;
+    opens io.hotmoka.network.internal.http.controllers to spring.core, spring.messaging;
     opens io.hotmoka.network.internal.websockets to spring.core, spring.context;
     requires org.apache.tomcat.embed.websocket;
 
