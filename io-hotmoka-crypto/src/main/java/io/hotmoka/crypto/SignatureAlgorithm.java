c9dbd82b9e13d49f5c30645d9abc29ee08e85ce9
Fausto Spoto <fausto.spoto@gmail.com>
2020-09-23 12:43:52 +0200

    Temporarily removed qTesla code

--- io-hotmoka-crypto/src/main/java/io/hotmoka/crypto/SignatureAlgorithm.java
+++ io-hotmoka-crypto/src/main/java/io/hotmoka/crypto/SignatureAlgorithm.java
@@ -11,7 +11,6 @@ import java.security.spec.InvalidKeySpecException;
 
 import io.hotmoka.crypto.internal.ED25519;
 import io.hotmoka.crypto.internal.Empty;
-import io.hotmoka.crypto.internal.QTESLA;
 import io.hotmoka.crypto.internal.SHA256DSA;
 
 /**
@@ -87,18 +86,6 @@ public interface SignatureAlgorithm<T> {
 	 */
 	static <T> SignatureAlgorithm<T> ed25519(BytesSupplier<? super T> supplier) throws NoSuchAlgorithmException {
 		return new ED25519<>(supplier);
-	}
-
-	/**
-	 * Yields the qTESLA-p-III signature algorithm.
-	 *
-	 * @param <T> the type of values that get signed
-	 * @param supplier how values get transformed into bytes, before being hashed and then signed
-	 * @return the algorithm
-	 * @throws NoSuchAlgorithmException if the installation does not include the ed25519 algorithm
-	 */
-	static <T> SignatureAlgorithm<T> qTesla(BytesSupplier<? super T> supplier) throws NoSuchAlgorithmException {
-		return new QTESLA<>(supplier);
 	}
 
 	/**
