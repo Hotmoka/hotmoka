847e64e9b52ef7a013a42a715dfd460ea0ea4220
Fausto Spoto <fausto.spoto@gmail.com>
2020-09-28 14:13:05 +0200

    Added missing class

---
+++ io-takamaka-examples/src/main/java/io/takamaka/tests/auction/Auction.java
@@ -0,0 +1,14 @@
+package io.takamaka.tests.auction;
+
+import io.takamaka.code.lang.Contract;
+import io.takamaka.code.lang.PayableContract;
+
+public abstract class Auction extends Contract {
+
+	/**
+	 * End the auction and send the highest bid to the beneficiary.
+	 * 
+	 * @return the winner of the action
+	 */
+	public abstract PayableContract auctionEnd();
+}
