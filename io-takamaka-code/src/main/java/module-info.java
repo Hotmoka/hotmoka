3358afc173abb6dc75a9a34b3fe2956e530d052c
Fausto Spoto <fausto.spoto@gmail.com>
2020-10-11 12:46:44 +0200

    Added events fro white/black listing accounts for @SelfCharged

--- io-takamaka-code/src/main/java/module-info.java
+++ io-takamaka-code/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module io.takamaka.code {
 	exports io.takamaka.code.lang;
-	exports io.takamaka.code.util;
+	exports io.takamaka.code.selfcharged;
 	exports io.takamaka.code.system;
+	exports io.takamaka.code.util;
 }
--- io-takamaka-examples/src/main/java/io/takamaka/tests/selfcharged/SelfChargeable.java
+++ io-takamaka-examples/src/main/java/io/takamaka/tests/selfcharged/SelfChargeable.java
@@ -1,9 +1,13 @@
 package io.takamaka.tests.selfcharged;
 
+import static io.takamaka.code.lang.Takamaka.event;
+
 import io.takamaka.code.lang.Contract;
 import io.takamaka.code.lang.Entry;
 import io.takamaka.code.lang.Payable;
 import io.takamaka.code.lang.SelfCharged;
+import io.takamaka.code.selfcharged.BlackList;
+import io.takamaka.code.selfcharged.WhiteList;
 
 public class SelfChargeable extends Contract {
 	public int i;
@@ -19,5 +23,16 @@ public class SelfChargeable extends Contract {
 	// this can be called also by an account with zero balance
 	public @SelfCharged void goo() {
 		i++;
+	}
+
+	/**
+	 * An example of generation of events for adding/removing from the white-list
+	 * of accounts that can call {@code @@SelfCharged} methods, if any such white-list exist.
+	 */
+	public void whiteOrBlack(int i, String account) {
+		if (i % 2 == 0)
+			event(new WhiteList(this, account));
+		else
+			event(new BlackList(this, account));
 	}
 }
