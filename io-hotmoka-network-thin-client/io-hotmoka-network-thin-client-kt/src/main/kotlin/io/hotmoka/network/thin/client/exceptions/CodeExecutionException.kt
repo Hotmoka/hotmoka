b07d2b128fddbc5c3a5c45381c66a698810a8d08
Dinu Berinde <dinu2193@yahoo.com>
2020-10-18 15:11:22 +0200

    porting to kotlin the add and run methods of the node

---
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/src/main/kotlin/io/hotmoka/network/thin/client/exceptions/CodeExecutionException.kt
@@ -0,0 +1,5 @@
+package io.hotmoka.network.thin.client.exceptions
+
+import java.lang.RuntimeException
+
+class CodeExecutionException(message: String): RuntimeException(message)
---
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/src/main/kotlin/io/hotmoka/network/thin/client/exceptions/TransactionException.kt
@@ -0,0 +1,5 @@
+package io.hotmoka.network.thin.client.exceptions
+
+import java.lang.RuntimeException
+
+class TransactionException(message: String): RuntimeException(message)
