97b18fc5effdec349e99266430d4c512d52d4dcf
Dinu Berinde <dinu2193@yahoo.com>
2020-11-10 22:14:47 +0100

    kotlin tests clean-up

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/src/test/kotlin/RemoteNodeTest.kt
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/src/test/kotlin/RemoteNodeTest.kt
@@ -37,140 +37,168 @@ class RemoteNodeTest {
 
 
     @test fun getTakamakaCode() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val takamakaCode = nodeService.getTakamakaCode()
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(takamakaCode, "expected result to be not null")
-        assertNotNull(takamakaCode.hash, "expected takamakaCode to be not null")
-        assertEquals(takamakaCode.type, "local", "expected takamakaCode type to be local")
+            val takamakaCode = service.getTakamakaCode()
+
+            assertNotNull(takamakaCode, "expected result to be not null")
+            assertNotNull(takamakaCode.hash, "expected takamakaCode to be not null")
+            assertEquals(takamakaCode.type, "local", "expected takamakaCode type to be local")
+        }
     }
 
     @test fun getManifest() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val reference = nodeService.getManifest()
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(reference, "expected result to be not null")
-        assertNotNull(reference.transaction, "expected transaction to be not null")
-        assertEquals(reference.transaction.type, "local", "expected transaction reference to be of type local")
-        assertNotNull(reference.transaction.hash, "expected transaction reference to be not null")
+            val reference = service.getManifest()
+
+            assertNotNull(reference, "expected result to be not null")
+            assertNotNull(reference.transaction, "expected transaction to be not null")
+            assertEquals(reference.transaction.type, "local", "expected transaction reference to be of type local")
+            assertNotNull(reference.transaction.hash, "expected transaction reference to be not null")
+        }
     }
 
     @test fun getState() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val manifestReference = nodeService.getManifest()
-        val state = nodeService.getState(manifestReference)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(state, "expected state to be not null")
-        assertEquals(state.updates.size, 2)
-        assertNotNull(state.updates[0].updatedObject, "expected updateObject to not null")
-        assertEquals(state.updates[0].updatedObject.transaction.hash, manifestReference.transaction.hash)
+            val manifestReference = service.getManifest()
+            val state = service.getState(manifestReference)
+
+            assertNotNull(state, "expected state to be not null")
+            assertEquals(state.updates.size, 2)
+            assertNotNull(state.updates[0].updatedObject, "expected updateObject to not null")
+            assertEquals(state.updates[0].updatedObject.transaction.hash, manifestReference.transaction.hash)
+        }
     }
 
     @test fun getStateNonExisting() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        try {
-            nodeService.getState(nonExistingStorageReference)
-        } catch (e: Exception) {
-            assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
-            assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
-            return
+            try {
+                service.getState(nonExistingStorageReference)
+            } catch (e: Exception) {
+                assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
+                assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
+                return
+            }
+
+            fail("expected exception")
         }
-
-        fail("expected exception")
     }
 
     @test fun getClassTag() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val manifestReference = nodeService.getManifest()
-        val takamakaCode = nodeService.getTakamakaCode()
-        val classTag = nodeService.getClassTag(manifestReference)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(classTag, "expected classTag to be not null")
-        assertEquals(classTag.className, "io.takamaka.code.system.Manifest")
-        assertNotNull(classTag.jar.hash, "expected classTag jar to be not null")
-        assertEquals(classTag.jar.hash, takamakaCode.hash, "expected classTag jar to be eq to the takamakaCode")
+            val manifestReference = service.getManifest()
+            val takamakaCode = service.getTakamakaCode()
+            val classTag = service.getClassTag(manifestReference)
+
+            assertNotNull(classTag, "expected classTag to be not null")
+            assertEquals(classTag.className, "io.takamaka.code.system.Manifest")
+            assertNotNull(classTag.jar.hash, "expected classTag jar to be not null")
+            assertEquals(classTag.jar.hash, takamakaCode.hash, "expected classTag jar to be eq to the takamakaCode")
+        }
     }
 
     @test fun getClassTagNonExisting() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        try {
-            nodeService.getClassTag(nonExistingStorageReference)
-        } catch (e: Exception) {
-            assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
-            assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
-            return
+            try {
+                service.getClassTag(nonExistingStorageReference)
+            } catch (e: Exception) {
+                assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
+                assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
+                return
+            }
+
+            fail("expected exception")
         }
-
-        fail("expected exception")
     }
 
     @test fun getRequest() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val takamakaCodeTransactionReference = nodeService.getTakamakaCode()
-        val transactionRequest = nodeService.getRequest(takamakaCodeTransactionReference)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(transactionRequest, "expected transactionRequest to be not null")
-        assertTrue(
-            transactionRequest.transactionResponseModel is JarStoreInitialTransactionRequestModel,
-            "expected transaction request model to be of type JarStoreInitialTransactionResponseModel"
-        )
+            val takamakaCodeTransactionReference = service.getTakamakaCode()
+            val transactionRequest = service.getRequest(takamakaCodeTransactionReference)
+
+            assertNotNull(transactionRequest, "expected transactionRequest to be not null")
+            assertTrue(
+                transactionRequest.transactionResponseModel is JarStoreInitialTransactionRequestModel,
+                "expected transaction request model to be of type JarStoreInitialTransactionResponseModel"
+            )
+        }
     }
 
 
     @test fun getRequestNonExisting() {
-        val nodeService: RemoteNode = RemoteNodeClient(url)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        try {
-            nodeService.getRequest(nonExistingTransactionReference)
-        } catch (e: Exception) {
-            assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
-            assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
-            return
+            try {
+                service.getRequest(nonExistingTransactionReference)
+            } catch (e: Exception) {
+                assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
+                assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
+                return
+            }
+
+            fail("expected exception")
         }
-
-        fail("expected exception")
     }
 
 
     @test fun getResponse() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
-        val takamakaCodeTransactionReference = nodeService.getTakamakaCode()
-        val transactionResponse = nodeService.getResponse(takamakaCodeTransactionReference)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        assertNotNull(transactionResponse, "expected transactionResponse to be not null")
-        assertTrue(
-            transactionResponse.transactionResponseModel is JarStoreInitialTransactionResponseModel,
-            "expected transaction response model to be of type JarStoreInitialTransactionResponseModel"
-        )
+            val takamakaCodeTransactionReference = service.getTakamakaCode()
+            val transactionResponse = service.getResponse(takamakaCodeTransactionReference)
+
+            assertNotNull(transactionResponse, "expected transactionResponse to be not null")
+            assertTrue(
+                transactionResponse.transactionResponseModel is JarStoreInitialTransactionResponseModel,
+                "expected transaction response model to be of type JarStoreInitialTransactionResponseModel"
+            )
+        }
     }
 
     @test fun getResponseNonExisting() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        try {
-            nodeService.getRequest(nonExistingTransactionReference)
-        } catch (e: Exception) {
-            assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
-            assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
-            return
+            try {
+                service.getRequest(nonExistingTransactionReference)
+            } catch (e: Exception) {
+                assertTrue(e is NoSuchElementException, "expected exception to of type NoSuchElementException")
+                assertTrue(e.message!!.equals("unknown transaction reference 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"))
+                return
+            }
+
+            fail("expected exception")
         }
-
-        fail("expected exception")
     }
 
     @test fun getPolledResponseNonExisting() {
-        val nodeService : RemoteNode = RemoteNodeClient(url)
+        val nodeService = RemoteNodeClient(url)
+        nodeService.use { service ->
 
-        try {
-            nodeService.getPolledResponse(nonExistingTransactionReference)
-        } catch (e: Exception) {
-            assertTrue(e is TimeoutException, "expected exception to of type TimeoutException")
-            return
+            try {
+                service.getPolledResponse(nonExistingTransactionReference)
+            } catch (e: Exception) {
+                assertTrue(e is TimeoutException, "expected exception to of type TimeoutException")
+                return
+            }
+
+            fail("expected exception")
         }
-
-        fail("expected exception")
     }
 
    /* @test fun addJarStoreTransaction() {
