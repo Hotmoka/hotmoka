aca5411306fd6ab8213b21b41c9d5abe66365989
Dinu Berinde <dinu2193@yahoo.com>
2020-11-11 01:33:10 +0100

    switched to kotlin Junit5 and installed the manifest for the test

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/build.gradle.kts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-kt/build.gradle.kts
@@ -10,10 +10,17 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation(kotlin("test-junit"))
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.2.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.google.code.gson:gson:2.8.6")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
