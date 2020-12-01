9058f98ce13f3631a7d850886ef0524deb8beb8f
Fausto Spoto <fausto.spoto@gmail.com>
2020-12-01 17:57:02 +0100

    Clean-up of the code for checking uses of caller()

--- io-takamaka-examples/src/main/java/io/takamaka/tests/errors/calleronthis/C.java
+++ io-takamaka-examples/src/main/java/io/takamaka/tests/errors/calleronthis/C.java
@@ -1,16 +1,13 @@
-package io.takamaka.tests.errors.callernotonthis;
+package io.takamaka.tests.errors.calleronthis;
 
 import io.takamaka.code.lang.Contract;
 import io.takamaka.code.lang.FromContract;
 
 public class C extends Contract {
+	@SuppressWarnings("unused")
 	private C owner;
 
 	public @FromContract(C.class) C() {
 		this.owner = (C) caller(); // ok
-	}
-
-	public @FromContract void m() {
-		owner.caller(); // ko
 	}
 }
