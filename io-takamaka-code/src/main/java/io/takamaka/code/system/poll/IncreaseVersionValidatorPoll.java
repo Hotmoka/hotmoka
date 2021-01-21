694704525937babef3225fa9de54f63762607341
Luca Olivieri <oli.luca93@gmail.com>
2021-01-21 23:26:14 +0100

    Fixed illegal call to non white listed method issues and added missing constructors

--- io-takamaka-code/src/main/java/io/takamaka/code/system/poll/IncreaseVersionValidatorPoll.java
+++ io-takamaka-code/src/main/java/io/takamaka/code/system/poll/IncreaseVersionValidatorPoll.java
@@ -1,11 +1,17 @@
 package io.takamaka.code.system.poll;
 
+import java.math.BigInteger;
+
 import io.takamaka.code.system.Manifest;
 
 public class IncreaseVersionValidatorPoll extends ValidatorPoll {
 	
 	public IncreaseVersionValidatorPoll(Manifest manifest) {
 		super(manifest);
+	}
+	
+	public IncreaseVersionValidatorPoll(BigInteger startTime, BigInteger durationTime, Manifest manifest) {
+		super(startTime, durationTime, manifest);
 	}
 
 	@Override
