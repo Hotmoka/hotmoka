9715213399f200cff5f47c6421a54bb6319a195a
Fausto Spoto <fausto.spoto@gmail.it>
2021-04-30 17:26:09 +0200

    Added license declaration at the beginning of the source files

--- io-hotmoka-tests/src/test/java/io/hotmoka/tests/errors/Exceptions.java
+++ io-hotmoka-tests/src/test/java/io/hotmoka/tests/errors/Exceptions.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 Fausto Spoto
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package io.hotmoka.tests.errors;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -41,7 +57,7 @@ class Exceptions extends TakamakaTest {
 		catch (Exception e) {
 			assertTrue(e instanceof TransactionException);
 			assertTrue(e.getMessage().startsWith(NullPointerException.class.getName()));
-			assertTrue(e.getMessage().endsWith("@C.java:9"));
+			assertTrue(e.getMessage().endsWith("@C.java:25"));
 		}
 	}
 
@@ -55,7 +71,7 @@ class Exceptions extends TakamakaTest {
 		catch (Exception e) {
 			assertTrue(e instanceof TransactionException);
 			assertTrue(e.getMessage().startsWith(NullPointerException.class.getName()));
-			assertTrue(e.getMessage().endsWith("@C.java:14"));
+			assertTrue(e.getMessage().endsWith("@C.java:30"));
 		}
 	}
 }
