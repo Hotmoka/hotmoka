69d672367e9fe597da58409800c2e8d480123b7a
Dinu Berinde <dinu2193@yahoo.com>
2021-05-16 22:14:07 +0200

    refactor

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/remote/RemoteNode.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/remote/RemoteNode.ts
@@ -4,29 +4,45 @@ import {ClassTagModel} from "../models/updates/ClassTagModel";
 import {StateModel} from "../models/updates/StateModel";
 import {TransactionReferenceModel} from "../models/values/TransactionReferenceModel";
 import axios, {AxiosResponse} from "axios";
+import {ErrorModel} from "../models/errors/ErrorModel";
 
 export class RemoteNode implements Node {
     readonly url: string
 
+    /**
+     * It constructs the instance of the remote node.
+     * @param url the url of the remote node
+     */
     constructor(url: string) {
         this.url = url
     }
 
-
-    private static invoke<T>(callback: () => Promise<T>) {
-        try {
-          return callback()
-        } catch (error) {
-            console.log(error)
-            throw error
-        }
+    /**
+     * It resolves the given error received from the HTTP call.
+     * @param error the error
+     * @private
+     * @return ErrorModel the parsed error model
+     */
+    private static resolveError(error): ErrorModel {
+        if (error && error.response && error.response.data && error.response.data.message)
+            return  new ErrorModel(error.response.data.message, error.response.data.exceptionClassName || 'Exception')
+        else
+            return new ErrorModel('Internal Server Error', 'Exception')
     }
 
-    private static invokeGet<T>(url: string) {
-        return RemoteNode.invoke(async () => {
+    /**
+     * Performs a GET request and yields a Promise of entity T as response.
+     * @param url the url
+     * @private
+     * @return a Promise of entity T
+     */
+    private static async get<T>(url: string): Promise<T> {
+        try {
             const res: AxiosResponse = await axios.get<T>(url)
             return res.data
-        })
+        } catch (error) {
+            throw RemoteNode.resolveError(error)
+        }
     }
 
     async getClassTag(request: StorageReferenceModel): Promise<ClassTagModel> {
@@ -45,6 +61,6 @@ export class RemoteNode implements Node {
     }
 
     async getTakamakaCode(): Promise<TransactionReferenceModel> {
-        return await RemoteNode.invokeGet<TransactionReferenceModel>(this.url + '/get/takamakaCode')
+        return await RemoteNode.get<TransactionReferenceModel>(this.url + '/get/takamakaCode')
     }
 }
