fae2ff447d8d8f7fbf6c9fb3c9c03fb54e679d53
Dinu Berinde <dinu2193@yahoo.com>
2021-05-21 11:15:36 +0200

    building hotmoka bundle for browser and node

---
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'hotmoka.js',
+        library: 'hotmoka',
+        libraryTarget:'umd',
+        globalObject: 'this'
+    },
+    target: 'node'
+};
