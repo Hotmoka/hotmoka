c47c922ed0d49332b17645a40960c67fb83dfc01
Dinu Berinde <dinu2193@yahoo.com>
2021-05-31 17:58:08 +0200

    marshalling the requests

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/InitializationTransactionRequestModel.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/InitializationTransactionRequestModel.ts
@@ -2,9 +2,18 @@ import {StorageReferenceModel} from "../values/StorageReferenceModel";
 import {TransactionReferenceModel} from "../values/TransactionReferenceModel";
 import {InitialTransactionRequestModel} from "./InitialTransactionRequestModel";
 import {MarshallingContext} from "../../internal/marshalling/MarshallingContext";
+import {Selectors} from "../../internal/marshalling/Selectors";
 
 export class InitializationTransactionRequestModel extends InitialTransactionRequestModel {
+    /**
+     * The reference to the jar containing the basic Takamaka classes. This must
+     * have been already installed by a previous transaction.
+     */
     manifest: StorageReferenceModel
+
+    /**
+     * The storage reference that must be set as manifest.
+     */
     classpath: TransactionReferenceModel
 
     constructor(manifest: StorageReferenceModel, classpath: TransactionReferenceModel) {
@@ -14,6 +23,8 @@ export class InitializationTransactionRequestModel extends InitialTransactionReq
     }
 
     protected into(context: MarshallingContext): void {
-        //TODO
+        context.writeByte(Selectors.SELECTOR_INITIALIZATION_TRANSACTION)
+        this.classpath.into(context)
+        this.manifest.intoWithoutSelector(context)
     }
 }
