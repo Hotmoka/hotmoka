5fb130b201ee0943568cdc77cfeb2b7ce2c3fdc4
Dinu Berinde <dinu2193@yahoo.com>
2021-05-31 18:00:05 +0200

    marshalling the requests

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/internal/marshalling/Selectors.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/internal/marshalling/Selectors.ts
@@ -70,5 +70,6 @@ export class Selectors {
     public static readonly SELECTOR_STATIC_METHOD_CALL = 6
     public static readonly SELECTOR_INSTANCE_SYSTEM_METHOD_CALL = 11
     public static readonly SELECTOR_INITIALIZATION_TRANSACTION = 10
+    public static readonly SELECTOR_GAMETE_CREATION = 2
 
 }
--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/GameteCreationTransactionRequestModel.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/GameteCreationTransactionRequestModel.ts
@@ -1,11 +1,28 @@
 import {TransactionReferenceModel} from "../values/TransactionReferenceModel";
 import {InitialTransactionRequestModel} from "./InitialTransactionRequestModel";
 import {MarshallingContext} from "../../internal/marshalling/MarshallingContext";
+import {Selectors} from "../../internal/marshalling/Selectors";
 
 export class GameteCreationTransactionRequestModel extends InitialTransactionRequestModel {
+    /**
+     * The amount of coin provided to the gamete.
+     */
     initialAmount: string
+
+    /**
+     * The amount of red coin provided to the gamete.
+     */
     redInitialAmount: string;
+
+    /**
+     * The Base64-encoded public key that will be assigned to the gamete.
+     */
     publicKey: string;
+
+    /**
+     * The reference to the jar containing the basic Takamaka classes. This must
+     * have been already installed by a previous transaction.
+     */
     classpath: TransactionReferenceModel
 
     constructor(initialAmount: string,
@@ -20,6 +37,10 @@ export class GameteCreationTransactionRequestModel extends InitialTransactionReq
     }
 
     protected into(context: MarshallingContext): void {
-        //TODO
+        context.writeByte(Selectors.SELECTOR_GAMETE_CREATION)
+        this.classpath.into(context)
+        context.writeBigInteger(Number(this.initialAmount))
+        context.writeBigInteger(Number(this.redInitialAmount))
+        context.writeString(this.publicKey)
     }
 }
