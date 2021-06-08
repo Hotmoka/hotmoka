423d15ca640782ce1e3305c197b58081f518b337
Dinu Berinde <dinu2193@yahoo.com>
2021-06-08 15:04:49 +0200

    clean-up

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/InstanceSystemMethodCallTransactionRequestModel.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/requests/InstanceSystemMethodCallTransactionRequestModel.ts
@@ -3,8 +3,6 @@ import {TransactionReferenceModel} from "../values/TransactionReferenceModel";
 import {MethodSignatureModel} from "../signatures/MethodSignatureModel";
 import {StorageValueModel} from "../values/StorageValueModel";
 import {MarshallingContext} from "../../internal/marshalling/MarshallingContext";
-import {Selectors} from "../../internal/marshalling/Selectors";
-import {Marshallable} from "../../internal/marshalling/Marshallable";
 import {AbstractInstanceMethodCallTransactionRequestModel} from "./AbstractInstanceMethodCallTransactionRequestModel";
 
 export class InstanceSystemMethodCallTransactionRequestModel extends AbstractInstanceMethodCallTransactionRequestModel {
@@ -23,13 +21,6 @@ export class InstanceSystemMethodCallTransactionRequestModel extends AbstractIns
     }
 
     protected into(context: MarshallingContext): void {
-        context.writeByte(Selectors.SELECTOR_INSTANCE_SYSTEM_METHOD_CALL)
-        this.caller.intoWithoutSelector(context)
-        context.writeBigInteger(Number(this.gasLimit))
-        this.classpath.into(context)
-        context.writeBigInteger(Number(this.nonce))
-        Marshallable.intoArray(this.actuals, context)
-        this.method.into(context)
-        this.receiver.intoWithoutSelector(context)
+       // nothing
     }
 }
