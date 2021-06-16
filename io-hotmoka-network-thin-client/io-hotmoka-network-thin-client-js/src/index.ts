f796c1b9b7c5a0eb970d9839b98a82689597a976
Dinu Berinde <dinu2193@yahoo.com>
2021-06-16 17:09:13 +0200

    exporting classes

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/index.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/index.ts
@@ -1,1 +1,24 @@
 export {RemoteNode} from "./internal/RemoteNode";
+export {Algorithm} from "./internal/signature/Algorithm";
+
+// requests
+export {InstanceMethodCallTransactionRequestModel} from "./models/requests/InstanceMethodCallTransactionRequestModel"
+export {ConstructorCallTransactionRequestModel} from "./models/requests/ConstructorCallTransactionRequestModel"
+export {StaticMethodCallTransactionRequestModel} from "./models/requests/StaticMethodCallTransactionRequestModel"
+export {JarStoreTransactionRequestModel} from "./models/requests/JarStoreTransactionRequestModel"
+export {JarStoreInitialTransactionRequestModel} from "./models/requests/JarStoreInitialTransactionRequestModel"
+
+// signatures
+export {ConstructorSignatureModel} from "./models/signatures/ConstructorSignatureModel"
+
+
+// values
+export {StorageValueModel} from "./models/values/StorageValueModel"
+export {StorageReferenceModel} from "./models/values/StorageReferenceModel"
+export {TransactionReferenceModel} from "./models/values/TransactionReferenceModel"
+
+// lang
+export {BasicType} from "./internal/lang/BasicType"
+export {ClassType} from "./internal/lang/ClassType"
+export {CodeSignature} from "./internal/lang/CodeSignature"
+export {Constants} from "./internal/lang/Constants"
