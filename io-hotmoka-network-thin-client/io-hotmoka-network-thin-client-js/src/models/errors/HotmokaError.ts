e8d820a68b82f5a992b5d30d473eead14066071b
Dinu Berinde <dinu2193@yahoo.com>
2021-06-23 17:57:15 +0200

    extending Error

--- io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/errors/HotmokaError.ts
+++ io-hotmoka-network-thin-client/io-hotmoka-network-thin-client-js/src/models/errors/HotmokaError.ts
@@ -1,8 +1,9 @@
-export class HotmokaError {
+export class HotmokaError extends Error {
     message: string
     exceptionClassName: string
 
     constructor(message: string, exceptionClassName: string) {
+        super(message)
         this.message = message
         this.exceptionClassName = exceptionClassName
     }
