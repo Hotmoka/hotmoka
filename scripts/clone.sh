4caa0d8d60be0f02af0a557b0de0d298ae48d86a
Fausto Spoto <fausto.spoto@gmail.com>
2022-11-04 17:28:45 +0100

    Completed init and clone scripts

---
+++ scripts/clone.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# an example of a script that runs a node of a blockchain
+# that clones and synchronizes with a remote node
+
+# source it as follows (to clone the node at panarea.hotmoka.io)
+# curl -s https://raw.githubusercontent.com/Hotmoka/hotmoka/master/scripts/clone.sh | bash -s hotmoka panarea.hotmoka.io
+# or (to specify the architecture)
+# curl -s https://raw.githubusercontent.com/Hotmoka/hotmoka/master/scripts/clone.sh | bash -s hotmoka panarea.hotmoka.io arm64
+
+TYPE=${1:-hotmoka}
+ARCH=${3:-""}
+
+TYPE_CAPITALIZED=${TYPE^}
+DIR=${TYPE}_node_info
+if [ $TYPE = hotmoka ];
+then
+    DOCKER_ID=hotmoka
+    NETWORK_URL=${2:-panarea.hotmoka.io}
+else
+    DOCKER_ID=veroforchain
+    NETWORK_URL=${2:-blueknot.vero4chain.it}
+fi;
+
+VERSION=$(curl --silent http://$NETWORK_URL/get/nodeID| python3 -c "import sys, json; print(json.load(sys.stdin)['version'])")
+
+if [ "$ARCH" = "" ];
+then
+    DOCKER_IMAGE=${DOCKER_ID}/tendermint-node:${VERSION}
+else
+    DOCKER_IMAGE=${DOCKER_ID}/tendermint-node-${ARCH}:${VERSION}
+fi;
+
+echo "Starting a node of the $TYPE_CAPITALIZED blockchain at $NETWORK_URL, version $VERSION:"
+docker rm $TYPE 2>/dev/null >/dev/null
+rm -r $DIR 2>/dev/null
+mkdir $DIR
+
+echo " * starting the docker container"
+docker run -dit --name $TYPE -p 80:8080 -p 26656:26656 -e NETWORK_URL=$NETWORK_URL -v chain:/home/$TYPE/chain $DOCKER_IMAGE start >/dev/null
+
+echo " * waiting for the node to complete its initialization"
+sleep 10
+echo "     waiting..."
+while !(docker exec $TYPE bash -c "mkdir -p extract; cp -f *.pem extract" 2>/dev/null)
+do
+    sleep 10
+    echo "     waiting..."
+done
+
+echo " * extracting the pem of the validator key"
+cd $DIR
+docker cp $TYPE:/home/$TYPE/extract/. .
+VALIDATOR_KEY=$(ls *.pem)
+ln -s $VALIDATOR_KEY validator_key.pem
+cd ..
+
+echo
+echo "The pem file of the key to control the node as validator"
+echo "(if it will ever be a validator) has been saved in the directory \"${DIR}\"."
+echo "Move that directory to the clients that need to control the node and delete it from this server."
+echo
+echo "The password of the validator key is empty."
---
+++ scripts/init.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# an example of a script that starts a brand new blockchain
+# initially consisting of a single node
+
+# source it as follows (to install version 1.0.10):
+# curl -s https://raw.githubusercontent.com/Hotmoka/hotmoka/master/scripts/init_hotmoka.sh | bash -s hotmoka 1.0.10
+# or (for a test network):
+# curl -s https://raw.githubusercontent.com/Hotmoka/hotmoka/master/scripts/init_hotmoka.sh | bash -s hotmoka 1.0.10 test
+# or (for a production network on arm64):
+# curl -s https://raw.githubusercontent.com/Hotmoka/hotmoka/master/scripts/init_hotmoka.sh | bash -s hotmoka 1.0.10 false arm64
+
+TYPE=${1:-hotmoka}
+VERSION=${2:-1.0.10}
+TEST=${3:-false}
+ARCH=${4:-""}
+
+TYPE_CAPITALIZED=${TYPE^}
+DIR=${TYPE}_node_info
+if [ $TYPE = hotmoka ];
+then
+    DOCKER_ID=hotmoka
+    CLI=moka
+    GITHUB_ID=Hotmoka
+    CHAIN_ID=marabunta
+    INITIAL_SUPPLY=10000000000000000000000000000000000
+else
+    DOCKER_ID=veroforchain
+    CLI=blue
+    GITHUB_ID=Vero4Chain
+    CHAIN_ID=octopus
+    INITIAL_SUPPLY=1000000000000000000000000000000000000000000000000
+fi;
+
+if [ "$ARCH" = "" ];
+then
+    DOCKER_IMAGE=${DOCKER_ID}/tendermint-node:${VERSION}
+else
+    DOCKER_IMAGE=${DOCKER_ID}/tendermint-node-${ARCH}:${VERSION}
+fi;
+
+echo "Starting the first node of a brand new $TYPE_CAPITALIZED blockchain version $VERSION:"
+docker rm $TYPE 2>/dev/null >/dev/null
+rm -r $DIR 2>/dev/null
+mkdir $DIR
+
+echo " * downloading the blockchain CLI"
+mkdir $DIR/$CLI
+cd $DIR/$CLI
+wget --quiet https://github.com/${GITHUB_ID}/${TYPE}/releases/download/v${VERSION}/${CLI}_${VERSION}.tar.gz
+tar zxf ${CLI}_${VERSION}.tar.gz
+cd ../..
+
+echo " * creating the key of the gamete"
+read -s -p "     choose a password for the gamete: " PASSWORD
+echo
+cd $DIR
+GAMETE_CREATION=$(./${CLI}/${CLI} create-key --password-of-new-key=$PASSWORD --interactive=false)
+cd ..
+LINE2=$(echo "$GAMETE_CREATION"| sed '2!d')
+GAMETE_PUBLIC_KEY_BASE58=${LINE2:19}
+
+echo " * starting the docker container"
+if [ $TEST = false ];
+then
+    docker run -dit --name $TYPE -e KEY_OF_GAMETE=$GAMETE_PUBLIC_KEY_BASE58 -e CHAIN_ID=${CHAIN_ID} -e INITIAL_SUPPLY=${INITIAL_SUPPLY} -p 80:8080 -p 26656:26656 -v chain:/home/${TYPE}/chain ${DOCKER_IMAGE} init >/dev/null
+else
+    docker run -dit --name $TYPE -e KEY_OF_GAMETE=$GAMETE_PUBLIC_KEY_BASE58 -e CHAIN_ID=${CHAIN_ID} -e INITIAL_SUPPLY=${INITIAL_SUPPLY} -e OPEN_UNSIGNED_FAUCET=true -p 80:8080 -p 26656:26656 -v chain:/home/${TYPE}/chain ${DOCKER_IMAGE} init >/dev/null
+fi;
+echo " * waiting for the node to complete its initialization"
+sleep 10
+echo "     waiting..."
+while !(docker exec ${TYPE} bash -c "mkdir -p extract; cp -f chain/*.pem extract" 2>/dev/null)
+do
+    sleep 10
+    echo "     waiting..."
+done
+
+echo " * extracting the pem of the validator account"
+cd $DIR
+docker cp ${TYPE}:/home/${TYPE}/extract/. .
+VALIDATOR_ACCOUNT=$(ls *#?.pem)
+VALIDATOR_ADDRESS=${VALIDATOR_ACCOUNT:0:66}
+ln -s $VALIDATOR_ACCOUNT validator.pem
+./${CLI}/${CLI} show-account $VALIDATOR_ADDRESS | tail -36 >${VALIDATOR_ADDRESS}_36_words.txt
+cd ..
+
+echo " * extracting the pem of the gamete account"
+cd $DIR
+GAMETE_BINDING=$(./${CLI}/${CLI} bind-key $GAMETE_PUBLIC_KEY_BASE58 --url localhost:80)
+LINE1=$(echo "$GAMETE_BINDING"| sed '1!d')
+GAMETE_ADDRESS=${LINE1:14:66}
+ln -s $GAMETE_ADDRESS.pem gamete.pem
+./${CLI}/${CLI} show-account $GAMETE_ADDRESS | tail -36 >${GAMETE_ADDRESS}_36_words.txt
+cd ..
+
+if [ $TEST != false ];
+then
+    echo " * opening an unsigned faucet"
+    cd $DIR
+    ./${CLI}/${CLI} faucet 10000000000000 --interactive=false --password-of-gamete=$PASSWORD --url localhost:80
+    cd ..
+fi;
+
+echo " * cleaning up"
+rm -r ${DIR}/${CLI}
+rm $DIR/$GAMETE_PUBLIC_KEY_BASE58.pem
+
+echo
+echo "The pem files to control the gamete and the validator have been saved in the directory \"${DIR}\"."
+echo "Move that directory to the clients that need to control the node and delete it from this server."
+echo
+echo "The password of the gamete is what you have chosen above."
+echo "The password of the validator is empty."
