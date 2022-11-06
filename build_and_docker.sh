6068d09f9daa80ca9571af1f2309223db29e92ee
Fausto Spoto <fausto.spoto@gmail.com>
2022-11-06 14:04:40 +0100

    Corrected script

--- build_and_docker.sh
+++ build_and_docker.sh
@@ -14,23 +14,26 @@ else
     VERSION=$1
 fi;
 
-echo $VERSION
-
 case $(uname -m) in
     arm64)
 	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node-arm64:${VERSION}
-    	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION};;
+   	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION}
+   	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node-arm64
+   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node-arm64;;
     aarch64)
 	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node-arm64:${VERSION}
-	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION};;
+   	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION}
+   	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node-arm64
+   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node-arm64;;
     x86_64)
 	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node:${VERSION}
-	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node:${VERSION};;
+	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node:${VERSION}
+	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node
+   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node;;
 esac
 
 mvn clean install -DskipTests
-docker build -t ${DOCKER_IMAGE_HOTMOKA} -f dockerfiles/tendermint-node/tendermint-node .
+docker build -t ${DOCKER_IMAGE_HOTMOKA} -f ${DOCKER_FILE_HOTMOKA} .
 docker push ${DOCKER_IMAGE_HOTMOKA}
-docker build -t ${DOCKER_IMAGE_BLUEKNOT} -f dockerfiles/tendermint-node/blueknot-tendermint-node .
+docker build -t ${DOCKER_IMAGE_BLUEKNOT} -f ${DOCKER_FILE_BLUEKNOT} .
 docker push ${DOCKER_IMAGE_BLUEKNOT}
-
