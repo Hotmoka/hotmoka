52a052e10200ff6a1003a4703c4ff5ea4ebace36
Fausto Spoto <fausto.spoto@hotmoka.io>
2023-03-19 17:10:49 +0400

    Started version 1.0.12

--- io-hotmoka-constants/src/main/java/io/hotmoka/constants/Constants.java
+++ io-hotmoka-constants/src/main/java/io/hotmoka/constants/Constants.java
@@ -24,7 +24,7 @@ public interface Constants {
 	/**
 	 * The version of the Hotmoka code.
 	 */
-	String VERSION = "1.0.11";
+	String VERSION = "1.0.12";
 
 	/**
 	 * The name of the class {@code io.hotmoka.local.internal.runtime.Runtime}.
