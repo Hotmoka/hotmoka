7f67f2705e9f6140c33261d34772941279856f9d
Fausto Spoto <fausto.spoto@hotmoka.io>
2023-05-16 10:35:22 +0200

    Removed stale scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This Maven invocation will compile and install all Hotmoka and Takamaka
-# jars in your local Maven repository. Remove -DskipTests if you
-# what to run the tests as well. Modules get compiled and
-# distributed inside the modules/ directory.
-
-mvn clean install -DskipTests
--- build_and_javadocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This Maven invocation will compile all Hotmoka and Takamaka
-# jars and generate their JavaDocs. Change the value for JAVA_HOME
-# to whatever is correct in your machine.
-
-JAVA_HOME=/usr/lib/jvm/default-java mvn clean install -Drestricted
