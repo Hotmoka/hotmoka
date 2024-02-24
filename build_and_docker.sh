6883ce823d8af902552187ae0f05237097d6a4bb
Fausto Spoto <fausto.spoto@hotmoka.io>
2024-02-24 22:23:43 +0100

    Updated docker scripts

--- build_and_docker.sh
+++ build_and_docker.sh
@@ -14,40 +14,18 @@ else
     VERSION=$1
 fi;
 
-case $(uname -m) in
-    arm64)
-	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node-arm64:${VERSION}
-   	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION}
-   	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node-arm64
-   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node-arm64;;
-    aarch64)
-	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node-arm64:${VERSION}
-   	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION}
-   	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node-arm64
-   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node-arm64;;
-    x86_64)
-	DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node:${VERSION}
-	DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node:${VERSION}
-	DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node
-   	DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node;;
-esac
-
 mvn clean install -DskipTests
 
 DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node:${VERSION}
 DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node:${VERSION}
 DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node
 DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node
-docker buildx build -t ${DOCKER_IMAGE_HOTMOKA} -f ${DOCKER_FILE_HOTMOKA} .
-docker push ${DOCKER_IMAGE_HOTMOKA}
-#docker buildx build -t ${DOCKER_IMAGE_BLUEKNOT} -f ${DOCKER_FILE_BLUEKNOT} .
-#docker push ${DOCKER_IMAGE_BLUEKNOT}
+docker buildx build --push --platform linux/amd64 -t ${DOCKER_IMAGE_HOTMOKA} -f ${DOCKER_FILE_HOTMOKA} .
+#docker buildx build --push --platform linux/amd64 -t ${DOCKER_IMAGE_BLUEKNOT} -f ${DOCKER_FILE_BLUEKNOT} .
 
 DOCKER_IMAGE_HOTMOKA=hotmoka/tendermint-node-arm64:${VERSION}
 DOCKER_IMAGE_BLUEKNOT=veroforchain/tendermint-node-arm64:${VERSION}
 DOCKER_FILE_HOTMOKA=dockerfiles/tendermint-node/tendermint-node-arm64
 DOCKER_FILE_BLUEKNOT=dockerfiles/tendermint-node/blueknot-tendermint-node-arm64
-docker buildx build -t ${DOCKER_IMAGE_HOTMOKA} -f ${DOCKER_FILE_HOTMOKA} .
-docker push ${DOCKER_IMAGE_HOTMOKA}
-#docker buildx build -t ${DOCKER_IMAGE_BLUEKNOT} -f ${DOCKER_FILE_BLUEKNOT} .
-#docker push ${DOCKER_IMAGE_BLUEKNOT}
+docker buildx build --push --platform linux/arm64 -t ${DOCKER_IMAGE_HOTMOKA} -f ${DOCKER_FILE_HOTMOKA} .
+#docker buildx build --push --platform linux/arm64 -t ${DOCKER_IMAGE_BLUEKNOT} -f ${DOCKER_FILE_BLUEKNOT} .
