dcc8d72edd984ac30fe14d3fd5a1c7255386da76
Fausto Spoto <fausto.spoto@hotmoka.io>
2024-04-16 10:04:41 +0200

    Fixed script

--- io-hotmoka-moka/moka.bat
+++ io-hotmoka-moka/moka.bat
@@ -2,4 +2,4 @@
 set SOURCE=%~dp0
 set DIR=%SOURCE%
 
-java --module-path "%DIR%modules\explicit";"%DIR%modules\automatic" --class-path "%DIR%modules\unnamed\*" --module io.hotmoka.moka/io.hotmoka.moka.Moka %*
+java --module-path "%DIR%modules\explicit";"%DIR%modules\automatic" --class-path "%DIR%modules\unnamed\*" --add-modules org.glassfish.tyrus.container.grizzly.server,org.glassfish.tyrus.container.grizzly.client --module io.hotmoka.moka/io.hotmoka.moka.Moka %*
